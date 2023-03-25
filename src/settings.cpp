88f227dca814e51f84cc2da4719a5892c5f06988
atctwo <atctwopointfive@gmail.com>
2023-03-25 17:47:38 +0000

    added callback registration to settings

--- src/settings.cpp
+++ src/settings.cpp
@@ -11,6 +11,19 @@
 // map to hold settings
 std::map< const char*, std::map< const char*, const char* > > settings;
 
+// struct for callback parameters
+struct callback_params {
+    const char *screen_name;
+    const char *setting_name;
+    void (*callback)(const char*);
+};
+
+// map to store callbacks
+// I used a map so callbacks could have a numeric ID and also be removed without the indexes
+// of all the other callbacks changing
+std::map<int, callback_params> callbacks;
+int next_callback_index = 0;
+
 void set_setting(const char *screen_name, const char *setting_name, const char *new_value)
 {
     // if screen name is passed as nullptr, use the current screen name
@@ -27,6 +40,22 @@ void set_setting(const char *screen_name, const char *setting_name, const char *
     {
         // tell the current screen about the update
         current_screen->setting_update(setting_name, new_value);
+    }
+
+    // call any callbacks registered for the setting
+    for (const std::pair<int, callback_params> param : callbacks) {
+
+        // if screen name and setting name match
+        if (strcmp(param.second.screen_name, screen_name_2) == 0) {
+            if (strcmp(param.second.setting_name, setting_name) == 0) {
+
+                // if callback isn't null
+                if (param.second.callback) param.second.callback(new_value);
+                else loge(LOG_TAG, "callback is null");
+
+            }
+        }
+
     }
 
 }
@@ -46,6 +75,25 @@ std::string get_setting(const char *screen_name, const char *setting_name, const
 
     // otherwise, return the setting value
     else return settings[screen_name_2][setting_name];
+}
+
+int register_setting_callback(const char *screen_name, const char *setting_name, void (*callback)(const char*))
+{
+    // if screen name is passed as nullptr, use the current screen name
+    const char *screen_name_2 = (screen_name == nullptr) ? current_screen_name() : screen_name;
+
+    logi(LOG_TAG, "registering callback for %d:%d (id=%d)", screen_name_2, setting_name, next_callback_index);
+
+    callback_params params = {screen_name_2, setting_name, callback};
+    callbacks[next_callback_index] = params;
+    
+    return next_callback_index++;
+}
+
+void remove_setting_callback(int callback_id)
+{
+    logi(LOG_TAG, "deregistering callback %d", callback_id);
+    callbacks.erase(callback_id);
 }
 
 std::vector<const char*> get_settings(const char *screen_name)
--- src/settings.h
+++ src/settings.h
@@ -15,6 +15,17 @@ void set_setting(const char *screen_name, const char *setting_name, const char *
 std::string get_setting(const char *screen_name, const char *setting_name, const char *default_value);
 
 /**
+ * Register a callback function to be called when a function changes
+*/
+int register_setting_callback(const char *screen_name, const char *setting_name, void (*callback)(const char*));
+
+/**
+ * Deregister a previously registered callback
+ * @param callback_id The id of the callback returned by `register_callback`
+*/
+void remove_setting_callback(int callback_id);
+
+/**
  * Returns a list of the names of each setting for a given screen
 */
 std::vector<const char*> get_settings(const char *screen_name);
